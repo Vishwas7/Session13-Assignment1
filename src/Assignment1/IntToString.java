b49ee567232b59d9e1dfc66152684a51527fb8dc
Vishwas7 <svishwas788@gmail.com>
2017-06-27 15:31:16 +0530

    Assignment13-1

---
+++ src/Assignment1/IntToString.java
@@ -0,0 +1,24 @@
+package Assignment1;
+
+public class IntToString {
+	
+public static void main(String[] args) {
+		
+		// In this program I've convert an int variable to String.
+		int x = 10;   // 10 is integer value
+		
+		
+		String a = String.valueOf(x); // This method will convert integer into String.
+			
+		  
+		System.out.println("Int value :"+ x); // integer value will display
+		
+		System.out.println("Int value convert into String :"+ x); // here converted  value will display 
+		                                                         // into String value. 
+
+	}
+
+}
+
+
+
